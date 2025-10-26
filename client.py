b5d5c5abc9b9a195e63a166b92527140d2f33e0c
shiraj0205 <shiraj.shaikh@gmail.com>
2025-10-26 09:52:20 -0500

    Tels the mcp by calling tools

--- client.py
+++ client.py
@@ -2,11 +2,13 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+load_dotenv()
+
 import asyncio
 import os
 
-load_dotenv()
 os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+
 
 async def main():
     """_summary_
@@ -34,15 +36,24 @@ async def main():
     tools = await client.get_tools()
     
     #model = ChatGroq(model="deepseek-r1-distill-llama-70b")
-    model = ChatGroq(model="qwen-qwq-32b")
+    model = ChatGroq(model="llama-3.1-8b-instant")
     
     agent = create_react_agent(model, tools)
+    # agent = AgentExecutor.from_agent_and_tools(
+    #     agent=create_json_chat_agent(llm=model, tools=tools),
+    #     tools=tools,
+    #     handle_parsing_errors=True
+    # )
     
     response = await agent.ainvoke(
         {"messages": [{"role": "user", "content": "what's (3 + 5) x 12?"}]}
     )
+
+    # response = await agent.ainvoke(
+    #     {"messages": [{"role": "user", "content": "Get weather in Paris?"}]}
+    # )
     
-    print(response[-1].content)
+    print(response["messages"][-1].content)
 
 
 
